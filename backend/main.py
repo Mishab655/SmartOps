c3c49e367c3168b19d4d546846e8c28922304273
Mishab <mishabmaruthil321@gmail.com>
2026-05-01 16:27:27 +0530

    implement index.html and changed answers generated

--- backend/main.py
+++ backend/main.py
@@ -4,10 +4,17 @@ import os
 
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 from backend.chatbot.executor import chatbot_answer
 
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Query(BaseModel):
     question: str
